74fa1676e88f151c9737c479bdce1eb1b3195d80
brianezhu <bzhu9992@yahoo.com>
2015-08-09 11:47:40 -0700

    first commit, pom.xml working, spring set up

---
+++ src/test/java/twelay/TwelayServer1ApplicationTests.java
@@ -0,0 +1,18 @@
+package twelay;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = TwelayServer1Application.class)
+@WebAppConfiguration
+public class TwelayServer1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
